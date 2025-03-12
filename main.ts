1755029ec1f53c531153cdd0895c63b45fae4b3d
21-NOK <194365042+21-NOK@users.noreply.github.com>
2025-03-12 10:29:49 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -742,8 +742,8 @@ namespace distance {
 //% weight=5 color=#63A4A2 icon="\uf032"
 namespace affichage {
     let display = Anneau_LED.create(DigitalPin.P2, 35)
-    let nbWidth = [4, 3, 5, 5, 5, 5, 5, 5, 5, 5]; // Largeur de la matrice en LEDs
-    let charwidth = [5, 5, 5, 5, 5, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
+    let nbWidth = [4, 2, 4, 4, 4, 4, 4, 4, 4, 4]; // Largeur de la matrice en LEDs
+    let charwidth = [4, 4, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
     let scrollSpeed = 300; // Temps entre chaque décalage 
     display.clear();
     display.show();
