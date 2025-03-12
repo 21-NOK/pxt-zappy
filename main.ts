6fd94af38a7ddf3c3224b69b78b95e44fad15b77
21-NOK <194365042+21-NOK@users.noreply.github.com>
2025-03-12 11:06:49 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -742,7 +742,7 @@ namespace distance {
 //% weight=5 color=#63A4A2 icon="\uf032"
 namespace affichage {
     let display = Anneau_LED.create(DigitalPin.P2, 35)
-    let nbWidth = [5, 3, 5, 5, 5, 5, 5, 5, 5, 5]; // Largeur de la matrice en LEDs
+    let nbWidth = [4, 2, 4, 4, 4, 4, 4, 4, 4, 4]; // Largeur de la matrice en LEDs
     let charwidth = [5, 5, 5, 5, 5, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];    let scrollSpeed = 300; // Temps entre chaque décalage 
     display.clear();
     display.show();
@@ -888,10 +888,11 @@ namespace affichage {
                 for (let index of letterPixels) {
                     let shiftedIndex = index + j;
 
-                    if (shiftedIndex == 3 || shiftedIndex == 8 || shiftedIndex == 14 || shiftedIndex == 21 || shiftedIndex == 27 || shiftedIndex == 32) {
+                    if (shiftedIndex%5 == 4) {
                         display.setPixelColor(shiftedIndex, Anneau_LED.colors(rgb));
                     }
                 }
+                shift(); // Décale toute la matrice
                 shift(); // Décale toute la matrice
             }
         }
