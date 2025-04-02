c876a7d05e9d5bc7793b4984b8878ddb4554effc
21-NOK <194365042+21-NOK@users.noreply.github.com>
2025-04-02 16:20:26 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -944,7 +944,7 @@ namespace affichage {
         display.setBrightness(luminosite);
     }
 
-    //% block="en %rgb=neopixel_colors| afficher"
+    //% block="afficher en %rgb=neopixel_colors"
     //% imageLiteral=1
     //% imageLiteralColumns=5
     //% imageLiteralRows=7
